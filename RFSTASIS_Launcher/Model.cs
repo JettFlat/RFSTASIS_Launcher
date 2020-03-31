d69746ff7e393529856ff331275eb805a6e5198c
Den <backupper97@gmail.com>
2020-03-31 23:03:22 +0300

    Tested md5

--- RFSTASIS_Launcher/Model.cs
+++ RFSTASIS_Launcher/Model.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Security.Cryptography;
 using System.IO;
+using System.Net;
 
 namespace RFSTASIS_Launcher
 {
@@ -10,33 +11,50 @@ namespace RFSTASIS_Launcher
     {
         static public void Start()
         {
-            Hash.CompareFiles();
+            //Hash.CompareFiles();
+            var test = Server.GetStream("http://update1.rfstasis.com/clientpatch/Adv.dll");
+            using var file2 = File.OpenRead(@"D:\WORK\RFSTASIS_Launcher\Adv.dll");
+            var res = MD5Hash.Compare(test, file2);
         }
-        public class Hash
+        public class Server
         {
-            static public void CompareFiles()
+            static public Stream GetStream(string Url)
             {
-                using (var file1 = File.OpenRead(@"D:\WORK\RFSTASIS_Launcher\Adv.dll"))
-                {
-                    using (var file2 = File.OpenRead(@"D:\WORK\RFSTASIS_Launcher\Adv2.dll"))
-                    {
-                        var res = HashCompare(file1, file2);
-                    }
-
-                }
+                HttpWebRequest Request = (HttpWebRequest)WebRequest.Create(Url);
+                HttpWebResponse Response = (HttpWebResponse)Request.GetResponse();
+                var str = Response.GetResponseStream();
+                return str;
             }
-            static bool HashCompare(Stream file1, Stream file2)
+            //public static IEnumerable ParseFiles(string Url)
+            //{
+
+            //    yield return;
+            //}
+        }
+        public class MD5Hash
+        {
+            public static bool Compare(Stream file1, Stream file2)
             {
-                using (var md5hash1 = MD5.Create())
+                try
                 {
-                    using (var md5hash2 = MD5.Create())
+                    using (var md5hash1 = MD5.Create())
                     {
-                        var hash1 = md5hash1.ComputeHash(file1);
-                        var hash2 = md5hash2.ComputeHash(file2);
-                        if (Convert.ToBase64String(hash1) == Convert.ToBase64String(hash2))
-                            return true;
-                        return false;
+                        using (var md5hash2 = MD5.Create())
+                        {
+                            var hash1 = md5hash1.ComputeHash(file1);
+                            var hash2 = md5hash2.ComputeHash(file2);
+                            if (Convert.ToBase64String(hash1) == Convert.ToBase64String(hash2))
+                                return true;
+                            return false;
+                        }
                     }
+                }
+                finally
+                {
+                    if (file1 != null)
+                        file1.DisposeAsync();
+                    if (file2 != null)
+                        file2.DisposeAsync();
                 }
             }
         }
