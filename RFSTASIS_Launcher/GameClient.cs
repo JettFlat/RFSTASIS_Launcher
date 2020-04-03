58195f2f054ddb5c1655a69e665e92a9b957cdd7
Den <backupper97@gmail.com>
2020-04-03 12:59:26 +0300

    New Replace

--- RFSTASIS_Launcher/GameClient.cs
+++ RFSTASIS_Launcher/GameClient.cs
@@ -1,10 +1,14 @@
-﻿using System;
+﻿using MiniLauncher.Helper;
+using MiniLauncher.Network;
+using MiniLauncher.Network.Packets;
+using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using static RFSTASIS_Launcher.Model;
 
@@ -12,6 +16,7 @@ namespace RFSTASIS_Launcher
 {
     public class GameClient
     {
+        static NetworkClient networkClient;
         public static string Path => Environment.CurrentDirectory;
         public static ConcurrentBag<FileInfoContainer> GetFilesHash()
         {
@@ -54,9 +59,79 @@ namespace RFSTASIS_Launcher
                 }
             }
         }
-        public static void RunGame()
+        public static void Start()
         {
+            Login();
+        }
+        public static void InitializeNetwork()
+        {
+            var serverCfg = Model.SettingsCur.Servercfg;
+            string ip = serverCfg.LogginAddress.Split(':')[0];
+            int port = int.Parse(serverCfg.LogginAddress.Split(':')[1]);
+            networkClient = new NetworkClient(ip, port);
+            networkClient.OnError += NetworkClient_OnError;
+            networkClient.OnConnected += NetworkClient_OnConnected;
+            networkClient.ClientEvents += NetworkClient_ClientEvents;
+            Task.Run(() => { networkClient.StartClient(); });
+        }
+        static void NetworkClient_OnConnected(object sender, EventArgs e)
+        {
+            //ChangeStatus(true);
+        }
+        static void NetworkClient_ClientEvents(object sender, NetworkClientEventArgs e)
+        {
+            switch (e.CState)
+            {
+                case NetworkClientEventArgs.Callback.CRYPTO_KEY_INFORM:
+                    //EnableLoginBtn(true);
+                    break;
+                case NetworkClientEventArgs.Callback.LOGIN_ACCOUNT_WRONG_LOGIN:
+                    //EnableLoginBtn(true);
+                    //MessageBox.Show("Wrong Login", "Error");
+                    break;
+                case NetworkClientEventArgs.Callback.LOGIN_ACCOUNT_WRONG_PW:
+                    //EnableLoginBtn(true);
+                    //MessageBox.Show("Wrong Password", "Error");
+                    break;
+                case NetworkClientEventArgs.Callback.LOGIN_ACCOUNT_SERVER_CLOSED:
+                    //EnableLoginBtn(true);
+                    //MessageBox.Show("Login Server Closed, Contact Admin", "Error");
+                    break;
+                case NetworkClientEventArgs.Callback.LOGIN_ACCOUNT_BANNED:
+                    //MessageBox.Show("Account Blocked", "Error");
+                    break;
+                case NetworkClientEventArgs.Callback.SERVER_LIST_INFORM:
+                    FillServerList(e.Servers);
+                    break;
+                case NetworkClientEventArgs.Callback.SERVER_SESSION_RESULT:
+                    RunGame(e.DefaultSet);
+                    break;
+            }
+        }
+        static void FillServerList(List<ServerState> _serverList)
+        {
+            Task.Run(()=>{ networkClient.SelectWordlRequest(Model.SettingsCur.Servercfg.ServerIndexSelect); });
 
+        }
+        static void NetworkClient_OnError(object sender, EventArgs e)
+        {
+          //  ChangeStatus(false);
+        }
+        static void RunGame(Default_Set defaultSet)
+        {
+            defaultSet.nation_code = 3;
+            ClientRunHelper.WriteTmp(@"System/DefaultSet.tmp", defaultSet);
+            ClientRunHelper.RunClient(@"RF_Online.bin");
+            networkClient.StopListen();
+            Environment.Exit(0);
+        }
+        static void Login()
+        {
+            Task.Run(() =>
+            {
+                Thread.Sleep(5000);
+                networkClient.DoLogin("jett", "NVcjk347dl");
+            });
         }
     }
 }
